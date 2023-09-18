7ae1ed35993eb3d6750917a601df7e16eae4ffed
Greta110051075 <113588842+Greta110051075@users.noreply.github.com>
2023-09-18 13:37:33 +0800

    9/18

--- Assets/Sprites/Run/MoveSystem.cs
+++ Assets/Sprites/Run/MoveSystem.cs
@@ -4,32 +4,26 @@ using UnityEngine;
 
 public class MoveSystem : MonoBehaviour
 {
-    public  Transform movePoint;
+    public Transform movePoint;
 
     [Header("左右速度")]
     [SerializeField, Range(10, 0)]
-    private  float speedHoruzontal =10f;
+    private float speedHoruzontal = 10f;
 
     [Header("速度")]
     [SerializeField, Range(10, 0)]
-    private  float speedVertical = 10f;
+    private float speedVertical = 10f;
 
     private void Update()
     {
         float h = Input.GetAxis("Horizontal");
+      
 
-        h = Mathf.Clamp(h, -26, 18);
+        transform.Translate(speedHoruzontal * Time.deltaTime * h, speedVertical * Time.deltaTime, 0);
 
-        transform.Translate(speedHoruzontal * Time.deltaTime * h, speedVertical*Time.deltaTime, 0);
 
-        //if (transform.position.x >= 13)
-        //{
-        //    transform.position = new Vector3(speedHoruzontal * Time.deltaTime* 13, speedVertical * Time.deltaTime, 0);
-        //}
-        //if (transform.position.x <= -21)
-        //{
-        //    transform.position = new Vector3(speedHoruzontal * Time.deltaTime * -21, speedVertical * Time.deltaTime, 0);
-        //}
+
+
 
 
     }
