c58ff39da0b0a2f143c120fb74b84fcbc4386961
Greta110051075 <113588842+Greta110051075@users.noreply.github.com>
2023-10-08 03:23:44 +0800

    10/8

--- Assets/Sprites/Run/WayPointFollow.cs
+++ Assets/Sprites/Run/WayPointFollow.cs
@@ -18,6 +18,19 @@ public class WayPointFollow : MonoBehaviour
     {
         if (circuit.Waypoints.Length == 0) return;
 
-        Vector3 lookAtGoal
+        Vector3 lookAtGoal = new Vector3(circuit.Waypoints[currentWP].transform.position.x, this.transform.position.y, circuit.Waypoints[currentWP].transform.position.z);
+        Vector3 direction = lookAtGoal - this.transform.position;
+        Vector3 directionVertical = lookAtGoal - circuit.Waypoints[currentWP].transform.position;
+        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), Time.deltaTime * rotSpeed);
+
+        if (direction.magnitude < closePoint)
+        {
+            currentWP++;
+            if (currentWP >= circuit.Waypoints.Length)
+            {
+                currentWP = 0;
+            }
+        }
+        this.transform.Translate(0, 0, speed * Time.deltaTime);
     }
 }
