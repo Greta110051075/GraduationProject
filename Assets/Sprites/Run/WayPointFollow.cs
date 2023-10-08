f1c9bc0be09167863c3325c93c7d0191fa0c15f3
Greta110051075 <113588842+Greta110051075@users.noreply.github.com>
2023-10-09 03:10:34 +0800

    10/9

--- Assets/Sprites/Run/WayPointFollow.cs
+++ Assets/Sprites/Run/WayPointFollow.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using Fungus;
+using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -11,18 +12,34 @@ public class WayPointFollow : MonoBehaviour
     public int currentWP = 0;
 
     public float speed = 5; //控制車速
-    public float closePoint = 0.5f; //控制距離路徑點最近距離
+    public float closePoint = 0f; //控制距離路徑點最近距離
     public float rotSpeed = 2; //控制旋轉速度
 
+ 
+    
     private void Update()
     {
+
+
+        //如果場景沒有任何 waypoint 則回傳 0 (不動作)
         if (circuit.Waypoints.Length == 0) return;
 
-        Vector3 lookAtGoal = new Vector3(circuit.Waypoints[currentWP].transform.position.x, this.transform.position.y, circuit.Waypoints[currentWP].transform.position.z);
+        //面像與目標的相關設定
+        Vector3 lookAtGoal = new Vector3(circuit.Waypoints[currentWP].transform.position.x,
+                                        this.transform.position.y,
+                                        circuit.Waypoints[currentWP].transform.position.z);
+
         Vector3 direction = lookAtGoal - this.transform.position;
         Vector3 directionVertical = lookAtGoal - circuit.Waypoints[currentWP].transform.position;
-        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), Time.deltaTime * rotSpeed);
+        Debug.DrawRay(this.transform.position, direction, Color.green);
+        Debug.DrawRay(circuit.Waypoints[currentWP].transform.position, directionVertical, Color.yellow);
 
+        //使用 Slerp 方法，使移動物件，旋轉朝向目標物
+        this.transform.rotation = Quaternion.Slerp(this.transform.rotation,
+                                                    Quaternion.LookRotation(direction),
+                                                    Time.deltaTime * rotSpeed);
+
+        // 追擊目標時的 waypoint 陣列索引號變更&歸零(重複)
         if (direction.magnitude < closePoint)
         {
             currentWP++;
@@ -31,6 +48,8 @@ public class WayPointFollow : MonoBehaviour
                 currentWP = 0;
             }
         }
-        this.transform.Translate(0, 0, speed * Time.deltaTime);
+
+        this.transform.Translate(0, 0, speed * Time.deltaTime); //物件向前移動方式
+
     }
 }
