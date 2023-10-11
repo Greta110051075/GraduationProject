82c9aa4dc5d204f96b5aa01ed5bb3bb2b301843c
Greta110051075 <113588842+Greta110051075@users.noreply.github.com>
2023-10-12 04:10:41 +0800

    10/12

---
+++ Assets/Sprites/GetCompTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GetCompTest : MonoBehaviour
+{
+    public GameObject image; //取得照片物件
+    public Texture2D imageChangeFemaleToMale; //定義要換的材質
+    public Texture2D imageChangMaleToFemale;
+
+    public void ChangeImageFemaleToMale()
+    {
+        image.GetComponent<RawImage>().texture = imageChangeFemaleToMale;
+    }
+
+    public void ChangeImageMaleToFemale()
+    {
+
+    }
+}
