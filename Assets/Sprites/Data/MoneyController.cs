9d8204134105f3ac486248cffc8d05289b78dec4
Greta110051075 <113588842+Greta110051075@users.noreply.github.com>
2023-10-16 15:13:08 +0800

    10/16-3

--- Assets/Sprites/Data/MoneyController.cs
+++ Assets/Sprites/Data/MoneyController.cs
@@ -2,28 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class MoneyController : MonoBehaviour
 {
-    public Text numberText;
-    public int number = 0;
+    public TextMeshProUGUI moneyText; //顯示所持金的ui
+    public int money; //玩家所持金額
 
-    void Start()
+    private void Start()
     {
-        UpdateNumberText();
+        UpdateMoneyText(); // 在 Start 方法中初始化 UI 文本
     }
 
-    public void IncreaseNumber()
+    public void UpdateMoneyText()
     {
-        number++;
-        UpdateNumberText();
-    }
-
-    void UpdateNumberText()
-    {
-        if (numberText != null)
-        {
-            numberText.text = "Number: " + number;
-        }
+        moneyText.text = "" + money.ToString(); // 更新 UI 文本顯示
+        Debug.Log("扣款成功");
     }
 }
